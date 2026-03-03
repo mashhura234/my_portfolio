e6cae04765b5698e00986507dc7ccf62f27cae58
Mashhura Nasrin Monika <thehappiness026@gmail.com>
2026-03-03 13:57:29 +0600

    change in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = () => (
   <section id="home" className="hero">
     <div className="container">
       <p className="welcome-text">Welcome to my portfolio</p>
-      <h1>Mst.Mashhura Nasrin Monika | <span className="highlight">CSE Student & Aspiring Data Professional.</span></h1>
+      <h1>Mst.Mashhura Nasrin Monika  </h1>
       <p className="sub-headline">
-        Building efficient solutions through code and design. Currently pursuing B.Sc. in CSE at Metropolitan University.
+       CSE Student & Aspiring Data Professional, Building efficient solutions through code and design. Currently pursuing B.Sc. in CSE at Metropolitan University.
       </p>
       {/* Buttons and Profile Picture text have been removed as requested */}
     </div>
